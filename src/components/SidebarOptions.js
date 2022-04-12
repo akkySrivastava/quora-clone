12c1d091a216a9c96ace15238029f910bede7217
katezhang1234 <kate.a.zhang@gmail.com>
2022-04-12 18:28:25 -0500

    Fixed icons on side bar so that they match the filter names

--- src/components/SidebarOptions.js
+++ src/components/SidebarOptions.js
@@ -17,10 +17,10 @@ function SidebarOptions() {
 
   return (
     <div className="sidebarOptions">
-      <SidebarOption src="https://pyxis.nymag.com/v1/imgs/160/7b8/be9b71674a92129013428120b1a0c699b7-howtogetintodrawing.rsquare.w700.jpg" title='Drawing' />
-      <SidebarOption src="https://cdn.shopify.com/s/files/1/2398/2457/t/19/assets/pf-9170c22c--DSC06111.jpg?v=1615866938" title='Painting' />
-      <SidebarOption src="https://qphs.fs.quoracdn.net/main-thumb-t-801-100-Sf8h894FXbQZQit0TeqDrrqS6xw6dwCQ.jpeg" title="Music" />
+      <SidebarOption src="https://cdn.shopify.com/s/files/1/2398/2457/t/19/assets/pf-9170c22c--DSC06111.jpg?v=1615866938" title='Visual Arts' />
       <SidebarOption src="https://marcolearning.com/wp-content/uploads/2022/02/different-kinds-of-writing-styles.jpg" title='Writing' />
+      <SidebarOption src="http://www.inquiriesjournal.com/imgs/1200x1200/crop/article-images/uid-170-1059163527-2886/92192b.jpg" title='Film' />
+      <SidebarOption src="https://qphs.fs.quoracdn.net/main-thumb-t-801-100-Sf8h894FXbQZQit0TeqDrrqS6xw6dwCQ.jpeg" title="Music" />
     </div>
   );
 }
