a22f9e5d5a919fc27ba5f202d8a46a225aaf7f8f
kotashashank <shashankkota@utexas.edu>
2022-04-21 19:29:49 -0500

    Sidebar stuff

--- src/components/SidebarOptions.js
+++ src/components/SidebarOptions.js
@@ -21,6 +21,7 @@ function SidebarOptions() {
       <SidebarOption src="https://marcolearning.com/wp-content/uploads/2022/02/different-kinds-of-writing-styles.jpg" title='Writing' />
       <SidebarOption src="http://www.inquiriesjournal.com/imgs/1200x1200/crop/article-images/uid-170-1059163527-2886/92192b.jpg" title='Film' />
       <SidebarOption src="https://qphs.fs.quoracdn.net/main-thumb-t-801-100-Sf8h894FXbQZQit0TeqDrrqS6xw6dwCQ.jpeg" title="Music" />
+      <SidebarOption src="https://qphs.fs.quoracdn.net/main-thumb-t-801-100-Sf8h894FXbQZQit0TeqDrrqS6xw6dwCQ.jpeg" title="All" />
     </div>
   );
 }
